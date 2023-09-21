1754d6c6bbc5f09c35716fdbeabb7a9766c0b4c6
Code Queen <channguyen97@gmail.com>
2023-09-21 15:14:43 -0700

    added boilerplate code for server

--- server.js
+++ server.js
@@ -1,15 +1,52 @@
-//imports
+//require express
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-// Helper method for generate unique ids
-const uuid = require()
+// Helper method for generate unique ids --to put in body of get/post requests functions!!!!!!
+const uuid = require();
 
 //boilerplate middleware
 const PORT = 3001;
-const app = express();
+const app = express(); //create instance of express
 
-app.use
+app.use(express.json()); //when we make requests, we can use json format AND
+app.use(express.urlencoded({ extended: true })); //when we make requests, we can use urlencoded format
 
-//server listener
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//server listener, the server's job is to listen for requests
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
