a022ccb23e1c6ca538b0ae434bd6c2e8c59a2f00
Code Queen <channguyen97@gmail.com>
2023-09-21 15:09:52 -0700

    added helpers folder and file, and started server file code

---
+++ server.js
@@ -0,0 +1,15 @@
+//imports
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+// Helper method for generate unique ids
+const uuid = require()
+
+//boilerplate middleware
+const PORT = 3001;
+const app = express();
+
+app.use
+
+//server listener
