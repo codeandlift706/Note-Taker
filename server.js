7585eeb056612608655de3f8ec124fe1590a8a90
Code Queen <channguyen97@gmail.com>
2023-09-21 20:41:52 -0700

    linked css and js files

--- server.js
+++ server.js
@@ -3,44 +3,44 @@ const express = require('express');
 const path = require('path'); //this is for...
 const fs = require('fs'); //this is for the readFile & writeToFile functions
 
-// Helper method for generate unique ids --to put in body of get/post requests functions!!!!!!
-const uuid = require('./Develop/helpers/uuid');
+// Helper method for generate unique ids --to put in body of get/post reqs functions!!!!!!
+const uuid = require('./helpers/uuid');
 
 //boilerplate middleware
 const PORT = 3001;
 const app = express(); //create instance of express
 
-app.use(express.json()); //when we make requests, we can use json format AND
-app.use(express.urlencoded({ extended: true })); //when we make requests, we can use urlencoded format
+app.use(express.json()); //when we make reqs, we can use json format AND
+app.use(express.urlencoded({ extended: true })); //when we make reqs, we can use urlencoded format
 
 app.use(express.static('public')); //so long as we have a file titled "index" in the public folder
 
 //this below is not needed if we have line 16
-app.get('/', (request, response) =>
-    response.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 //The following HTML routes should be created:
 
+//GET /notes should return the notes.html file.
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
 //GET * should return the index.html file.
 // * is a wildcard, will return the index.html as indicated by "/"
-app.get('*', (request, response) => {
-    response.send('<a href="/">Oopsie daisy! Nothing to see here. Navigate back to the homepage?</a>');
+app.get('*', (req, res) => {
+    res.send('<a href="/">Oopsie daisy! Nothing to see here. Navigate back to the homepage?</a>');
 });
-
-//GET /notes should return the notes.html file.
-app.get('/notes', (request, response) => {
-    response.send('<a href="/notes">See notes here.</a>');
-});
-
 
 //The following API routes should be created:
 
 //GET /api/notes should read the db.json file and return all saved notes as JSON.
-app.get('/api/notes', (request, response) => {
+app.get('/api/notes', (req, res) => {
 
-    //destructure for the items in request.body
-    const { title, text } = request.body
+    //destructure for the items in req.body
+    const { title, text } = req.body
 
     //if all the required properties are present,
     if (title && text) {
@@ -49,7 +49,7 @@ app.get('/api/notes', (request, response) => {
         const newNote = {
             title,
             text,
-            review_id: uuid(),
+            id: uuid(),
         };
 
         //if we have the required properties, then we can obtain existing notes
@@ -75,23 +75,24 @@ app.get('/api/notes', (request, response) => {
                 */
             }
         });
-}});
+    }
+});
 
 
-//POST REQUEST to add a note
-/*POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+//POST req to add a note
+/*POST /api/notes should receive a new note to save on the req body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 */
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (req, res) => {
 
-    //send a message to the client that request has been received to post a note
-    response.status(200).json(`${request.method} request received to post a note.`);
+    //send a message to the client that req has been received to post a note
+    res.status(200).json(`${req.method} request received to post a note.`);
 
-    //log this request to the terminal that the client has requested to post a note
-    console.info(`${request.method} request received to post a note.`)
+    //log this req to the terminal that the client has reqed to post a note
+    console.info(`${req.method} request received to post a note.`)
 
-    //destructure for the items in request.body
-    const { title, text } = request.body
+    //destructure for the items in req.body
+    const { title, text } = req.body
 
     //if all the required properties are present,
     if (title && text) {
@@ -100,7 +101,7 @@ app.post('/api/notes', (request, response) => {
         const newNote = {
             title,
             text,
-            review_id: uuid(),
+            id: uuid(),
         };
 
 
@@ -123,16 +124,16 @@ app.post('/api/notes', (request, response) => {
             }
         });
 
-        const response = {
+        const res = {
             status: 'success',
             body: newNote,
         };
 
 
-        console.log(response);
-        response.status(201).json(response);
+        console.log(res);
+        res.status(201).json(res);
     } else {
-        response.status(500).json('Error in posting note');
+        res.status(500).json('Error in posting note');
     }
 });
 
@@ -157,7 +158,7 @@ app.post('/api/notes', (request, response) => {
 
 
 
-    //server listener, the server's job is to listen for requests
-    app.listen(PORT, () =>
-        console.log(`App listening at http://localhost:${PORT}`)
-    );
+//server listener, the server's job is to listen for reqs
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
